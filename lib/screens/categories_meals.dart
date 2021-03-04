ebc0aab1bcf4a350d1c281fbc087a93e5a939324
Ismael Pedro <http.ismaeel@gmail.com>
2021-03-04 11:16:22 -0300

    Criação da tela de refeicoes e paginacao

---
+++ lib/screens/categories_meals.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import '../models/category.dart';
+import '../data/dummy_data.dart';
+import '../components/meal_item.dart';
+
+class CategoriesMeals extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final category = ModalRoute.of(context).settings.arguments as Category;
+
+    final categoryMeals = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(category.id);
+    }).toList();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(category.title),
+        centerTitle: true,
+      ),
+      body: ListView.builder(
+        itemCount: categoryMeals.length,
+        itemBuilder: (context, index) {
+          return MealItem(categoryMeals[index]);
+        },
+      ),
+    );
+  }
+}
